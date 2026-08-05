b7af8aa1f20728d07cbf9de8e064a5c56d14702f
Walcygleicson <walcygleicsonoliveira@gmail.com>
2026-08-05 20:29:09 -0300

    [refactor] Replaces the default type of key values from ny to unknown.

--- src/objects/PlainObject.ts
+++ src/objects/PlainObject.ts
@@ -2,21 +2,21 @@
  * Represents a generic object whose keys can be **`string`** or **`symbol`** with optionally typed values.
  *
  * ---
- * @template A The type of the key values *`[x: string]`*. The default is **`any`**.
+ * @template A The type of the key values *`[x: string]`*. The default is **`unknown`**.
  * @template B The type of the values for keys *`[x: symbol]`*. By default, it assumes the same type as **`A`**.
  *  @example
  * const Sym = Symbol()
  *
- * // Any “string” or “symbol” key with any type of value.
- * const a: PlainObject = {name: “John”, [Sym]: 123}
+ * // Any “string” or “symbol” key with an unknown value type.
+ * const a: PlainObject = {name: "John", [Sym]: 123}
  *
  * // Any key with a value of type “number” only.
  * const b: PlainObject<number> = {age: 25, [Sym]: 123} // ✅
- * const c: PlainObject<number> = {name: “John”, [Sym]: 123} // ❌ The ‘string’ type cannot be assigned to the “number” type.
+ * const c: PlainObject<number> = {name: "John", [Sym]: 123} // ❌ The ‘string’ type cannot be assigned to the “number” type.
  *
  * // Any “string” key with a “number” value and any ‘symbol’ key with a “boolean” value only.
  * const d: PlainObject<number, boolean> = {age: 25, [Sym]: true} // ✅
- * const e: PlainObject<number, boolean> = {name: “John”, age: 25, [Sym]: true} // ❌ The ‘string’ type cannot be assigned to the “number” type.
+ * const e: PlainObject<number, boolean> = {name: "John", age: 25, [Sym]: true} // ❌ The ‘string’ type cannot be assigned to the “number” type.
  */
 export type PlainObject<A = unknown, B = A> = {
   [x: string]: A;
