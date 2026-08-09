42d6bcccbb0b3407761271fcfaad2f415121fd49
Walcygleicson <walcygleicsonoliveira@gmail.com>
2026-08-09 12:26:58 -0300

    [fix] Makes the T parameter required. Previously, with T = unknown, omitting it simply resulted in unknown, which was not the expected behavior.

--- src/union/Truthy.ts
+++ src/union/Truthy.ts
@@ -2,6 +2,13 @@ import type { Falsy } from "./Falsy";
 
 /**
  * Removes the **`Falsy`** types from a `type union`.
+ * 
+ * ---
+ * @template T Base type to be filtered.
+ * @example
+ * type Mix = true | 0 | 1 | null | undefined | "hello"
+ * 
+ * type OnlyTruthy = Truthy<Mix> // true | 1 | "hello"
  */
-export type Truthy<T = unknown> = Exclude<T, Falsy>;
+export type Truthy<T> = Exclude<T, Falsy>;
 
