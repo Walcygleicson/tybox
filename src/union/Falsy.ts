869e35d9ccc604fa33dbac3df3cd374e0551f942
Walcygleicson <walcygleicsonoliveira@gmail.com>
2026-08-09 11:58:30 -0300

    [feat] Adds a new utility type Falsy.

---
+++ src/union/Falsy.ts
@@ -0,0 +1,4 @@
+/**
+ * Union of **`falsy`** types.
+ */
+export type Falsy = false | 0 | '' | null | undefined;
--- src/union/index.ts
+++ src/union/index.ts
@@ -1,1 +1,3 @@
 export type * from "./TypedArray"
+export type * from "./Primitive"
+export type * from "./Falsy"
