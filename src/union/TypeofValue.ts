2c62cf50741926e23440f2b06fc8799b3cff5742
Walcygleicson <walcygleicsonoliveira@gmail.com>
2026-08-09 13:08:21 -0300

    [feat] Adds a new utility type, TypeofValue: A union of string literals returned by the 	ypeof operator.

---
+++ src/union/TypeofValue.ts
@@ -0,0 +1,13 @@
+
+/**
+ * Union of string literals returned by the `typeof` operator.
+ */
+export type TypeofValue =
+  | 'string'
+  | 'number'
+  | 'bigint'
+  | 'boolean'
+  | 'symbol'
+  | 'undefined'
+  | 'object'
+  | 'function';
